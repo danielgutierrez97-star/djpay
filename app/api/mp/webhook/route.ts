2769ada274541d5a7849eedd86c34e5465ca9814
FRANKLIN <franklin@192.168.1.6>
2026-06-28 16:18:55 -0400

    Arreglar webhook temporal

---
+++ app/api/mp/webhook/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    console.log("WEBHOOK RECIBIDO:");
+    console.log(body);
+
+    return NextResponse.json({
+      success: true,
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      {
+        success: false,
+      },
+      { status: 500 }
+    );
+  }
+}
